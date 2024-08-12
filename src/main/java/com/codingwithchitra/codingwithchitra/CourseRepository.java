7deea0ab3903132838ef861a6d79a9c571e87f01
codingwithchitra <168246103+CodingWithChitra@users.noreply.github.com>
2024-08-12 09:29:46 +0000

    adding telegram bot

--- src/main/java/com/codingwithchitra/codingwithchitra/CourseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codingwithchitra.codingwithchitra;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
